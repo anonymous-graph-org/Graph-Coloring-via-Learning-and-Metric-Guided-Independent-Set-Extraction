667a33b8e9f6b2083d3eb7f1e236b285b61f8271
graph-chromatic <257513449+graph-chromatic@users.noreply.github.com>
2026-01-27 18:49:50 +0530

    Update main.py

--- Quantum circuit depth optimization/main.py
+++ Quantum circuit depth optimization/main.py
@@ -4,41 +4,18 @@ from src.load_model import load_model
 from src.model import DeepGCN
 import networkx as nx
 
-model_path = "/Users/ACER/Desktop/basicMIS_multiplesolns/gcn_model.pth"
-graphs_dict = load_graphs("/Users/ACER/PycharmProjects/Mixed-Graph-Coloring-Algorithms/quantum_circuit/qasm_graphs_original.pkl")
+model_path = "gcn_model.pth"
+graphs_dict = load_graphs("qasm_graphs_original.pkl")
 output_file = 'output.txt'
 hidden_dim = 32
 num_layers = 20
 model = load_model(model_path, DeepGCN, hidden_dim, num_layers)
 
-"""G = nx.DiGraph()
-
-# Add nodes
-G.add_node("G1")
-G.add_node("G2")
-G.add_node("G3")
-G.add_node("G4")
-G.add_node("G5")
-
-# Add directed edge A -> B
-G.add_edge("G1", "G3", directed=False)
-G.add_edge("G1", "G4", directed=True)
-G.add_edge("G2", "G3", directed=False)
-G.add_edge("G2", "G4", directed=True)
-G.add_edge("G2", "G5", directed=True)
-G.add_edge("G3", "G4", directed=True)
-
-# Print edges with attributes
-print("Edges in the graph:")
-for u, v, d in G.edges(data=True):
-    print(f"{u} -> {v}, directed = {d.get('directed')}")
-
-colors,coloring=predict_colors(G,model)
-print(colors) """
 
 with open(output_file, 'a', buffering=1) as file:
     for i, (name, g) in enumerate(graphs_dict.items()):
           colors, coloring = predict_colors(g,model)
           print(colors)
           file.write(name+"\n")
+
           file.write("predicted depth = "+str(colors)+"\n")
